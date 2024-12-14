3a0ec0b36c9e2bfe66b4ffa6de9ba418f66d85cd
Dody <prasetyodody17@gmail.com>
2024-12-15 05:49:28 +0700

    feat: sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://dodycode.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://dodycode.com/experience",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://dodycode.com/work",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
